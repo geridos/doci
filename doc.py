71b84d668ef1c801ecac4eef0eaa505a44eea043
bertrand <bertrand@localhost.localdomain>
2018-08-01 11:48:33 +0400

    print tree path for title

--- doc.py
+++ doc.py
@@ -61,7 +61,7 @@ def get_config(f):
             print("config file is not well formated :: " + l)
             sys.exit()
     f.close()
-    Conf.print_config()
+    #Conf.print_config()
     Conf.check_param()
 
 def strncmp(str1, str2, n):
@@ -80,27 +80,135 @@ def remove_space(line):
     return line[i:]
 
 def tag_match_line(tags, line):
-    match = False
     for tag in tags:
         if strncmp(tag, remove_space(line), len(tag)):
-            match = True
             return (True, tag)
     return (False, None)
 
 ##[definition][comments]
 ##format1 : <tag>[section][subsection][...](BLOCK|LINE|FILE|ENDTAG)
 
-class Section
+
+
+
+class Section:
+    def __init__(self, name):
+        self.section_name = name
+        self.com_data = []
+        self.file_data = []
+        self.mode = 0
+        self.currently_used = False
+        self.sections = []
+
+
+class Section_Manager():
+    def __init__(self):
+        self.entry_point_section = Section("ENTRY NODE")
+        self.current_section = None
+
+    def add_node(self, parent, sections):
+        new_section = Section(sections[0])
+        new_section.name = sections[0]
+        parent.sections.append(new_section)
+        if len(sections[1:]) > 0:
+            return self.add_node(new_section, sections[1:])
+        else:
+            #print("end node")
+            new_section.currently_used = True
+            self.current_section = new_section
+
+    def create_section(self, entrypoint, sections):
+        for s in entrypoint.sections:
+            if s.section_name == sections[0]:
+                return self.create_section(s, sections[1:])
+
+        self.add_node(entrypoint, sections)
+
+    def add(self, sections, mode):
+        sec = self.find(self.entry_point_section, sections)
+
+        #if sec:
+        #    print(sec.section_name)
+
+        if self.current_section:
+            self.current_section.currently_used = False;
+
+        if sec:
+            sec.current_section = True
+            self.current_section = sec
+        else:
+            self.create_section(self.entry_point_section, sections)
+
+    def find(self, entry, sections):
+        for n in entry.sections:
+            for s in sections:
+                if n.section_name == s:
+                    if len(sections) == 1:
+                        return n
+                    return self.find(n, sections[1:])
+        return None
+
+    def add_data(self, data):
+        sec = self.current_section
+        if sec == None:
+            print("No current section found data will be add without section")
+            #add data
+        else:
+            sec.com_data.append(data)
+
+    def print_tree(self):
+        self.print_node(self.entry_point_section, 0)
+
+
+    def print_node(self, entry, deepth):
+        print(('\t' * deepth) + entry.section_name + "(" + str(int(entry.currently_used)) +  " " + str(len(entry.sections)) + ") " + str(entry.com_data))
+        deepth += 1
+
+        for e in entry.sections:
+            self.print_node(e, deepth)
+
+
+
+
+
+sec_mana = Section_Manager()
+
 
 class Comment:
     def __init__(self, line):
         self.line = line
+        self.com_data = ""
+        self.parametres = []
+        self.mode = []
+        self.nb_lines = 0
+        self.parse()
 
-    def get_section:
+    def check_mode(self):
+        if self.mode in {'BLOCK', 'LINE', 'FILE', 'ENDTAG'}:
+            return True
+        return False
 
 
-
-    def get_lines
+    def parse(self):
+        #check if line is empty
+        if len(self.line) > 0 and self.line[0] == '[':
+            val = self.line.split('[', 10)      #get the content within [ ]
+            for v in val:
+                tmp = (v.split(']', 10))
+                for t in tmp:
+                    if t:                       #remove all the empty item
+                        self.parametres.append(t)
+            #print(self.parametres)
+            deepth = 0
+            sections = []
+            for p in self.parametres:
+                if p[0] == '(':
+                    self.mode = (p[1:].split(')', 1))
+                else:
+                    sections.append(p)
+            sec_mana.add(sections, self.mode)
+        else:
+            sec_mana.add_data(self.line)
 
 
 def parse_file(f):
@@ -110,16 +218,13 @@ def parse_file(f):
         if tag[0] == False:
             continue
         else:
-            print("tag found!")
-            Comment comment = Comment(line)
-
-
+            comment = Comment(line.strip()[len(tag[1]):])
 
 
 def process_files():
     global Conf
     for f in Conf.input_files:
-        print(f)
+        #print(f)
         try:
             f = open(f, 'r')
             parse_file(f)
@@ -131,3 +236,4 @@ if __name__ == '__main__':
     f = format_command_line()
     get_config(f)
     process_files()
+    sec_mana.print_tree()
